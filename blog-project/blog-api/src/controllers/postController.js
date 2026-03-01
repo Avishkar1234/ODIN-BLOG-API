eb28c549b40e688bf49185758fbd4fa7f444556c
Avishkar <avishkar1234567890@gmail.com>
2026-03-01 21:52:09 +0530

    feat: implement post creation and fetching logic

---
+++ blog-project/blog-api/src/controllers/postController.js
@@ -0,0 +1,55 @@
+const prisma = require("../config/prisma");
+
+exports.createPost = async (req, res) => {
+    try {
+        const { title, content } = req.body;
+
+        if (!title || !content) {
+            return res.status(400).json({ message: "Title and content required" });
+        }
+
+        const post = await prisma.post.create({
+            data: {
+                title,
+                content,
+                authorId: req.user.userId,
+            },
+        });
+
+        res.status(201).json(post);
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+//PUBLIC: Get all published posts
+exports.getPublishedPoss = async (req, res) => {
+    try {
+        const posts = await prisma.post.findMany({
+            where: { published: true },
+            include: {
+                author: true,
+            }
+        });
+
+        res.json(posts);
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+// ADMIN: Get all posts (including unpublished)
+exports.getAllPosts = async (req, res) => {
+    try {
+        const posts = await prisma.post.findMany({
+            include: { author: true },
+        });
+
+        res.json(posts);
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
