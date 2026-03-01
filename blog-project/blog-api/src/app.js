ebb8ce99e7b56f6a571c9bc0f57ca037fd440620
Avishkar <avishkar1234567890@gmail.com>
2026-03-02 00:20:53 +0530

    chore: remove temporary development test routes

--- blog-project/blog-api/src/app.js
+++ blog-project/blog-api/src/app.js
@@ -4,30 +4,18 @@ const cors = require("cors");
 
 const app = express();
 const authRoutes = require("./routes/authRoutes.js");
-const authMiddleware = require("./middleware/authMiddleware.js");
-const authorizeAdmin = require("./middleware/authorizeAdmin.js");
-const authenticateToken = require("./middleware/authMiddleware.js");
 const postRoutes = require("./routes/postRoutes.js");
-const commentRoutes = require("./routes/commentRoutes.js")
+const commentRoutes = require("./routes/commentRoutes.js");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
-app.use("/api", commentRoutes)
-
-app.get("/api/protected", authMiddleware, (req, res) => {
-    res.json({
-        message: "You accessed a protected route",
-        user: req.user
-    });
-});
-
+app.use("/api", commentRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "API is running" });
 });
-
 
 module.exports = app;
