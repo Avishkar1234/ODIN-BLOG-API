237e527ac6c892599bee3efa3424a9b7c864c207
Avishkar <avishkar1234567890@gmail.com>
2026-03-01 18:57:27 +0530

    feat: implement user registration and login with JWT

---
+++ blog-project/blog-api/src/controller/authController.js
@@ -0,0 +1,62 @@
+const bcyrpt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../config/prisma.js");
+
+exports.register = async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+
+        if (!email || !username || !password) {
+            return res.status(400).json({ message: "All fields required" });
+        }
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        const hashedPassword = await bcyrpt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                email,
+                username,
+                password: hashedPassword,
+            }
+        });
+
+        res.status(201).json({ message: "Server error" });
+    } catch (error) {
+        
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const validPassword = await bcyrpt.compare(password, user.password);
+
+        if (!validPassword) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { userId: user.id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        res.json({ token });
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
