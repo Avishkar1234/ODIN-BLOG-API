224c1139f099d93aaf3f0635f99b3b84c6234827
Avishkar <avishkar1234567890@gmail.com>
2026-03-01 20:29:41 +0530

    test: add protected route to verify JWT middleware

--- blog-project/blog-api/src/app.js
+++ blog-project/blog-api/src/app.js
@@ -4,16 +4,22 @@ const cors = require("cors");
 
 const app = express();
 const authRoutes = require("./routes/authRoutes.js");
+const authMiddleware = require("./middleware/authMiddleware.js");
 
 app.use(cors());
 app.use(express.json());
 
-//Middlewares
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-    res.json({ message: "API is running"});
+app.get("/api/protected", authMiddleware, (req, res) => {
+    res.json({
+        message: "You accessed a protected route",
+        user: req.user
+    });
 });
 
+app.get("/", (req, res) => {
+    res.json({ message: "API is running" });
+});
 
 module.exports = app;
