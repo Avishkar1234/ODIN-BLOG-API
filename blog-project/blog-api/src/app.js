28b34eddd45a97828dee1faea93a5a581822b4ef
Avishkar <avishkar1234567890@gmail.com>
2026-03-01 18:45:36 +0530

    feat: configure Prisma client connection

--- blog-project/blog-api/src/app.js
+++ blog-project/blog-api/src/app.js
@@ -3,11 +3,18 @@ const cors = require("cors");
 
 const app = express();
 
+const prisma = require("./config/prisma");
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "API is running"});
 });
+
+app.get("/test-db", async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+})
 
 module.exports = app;
