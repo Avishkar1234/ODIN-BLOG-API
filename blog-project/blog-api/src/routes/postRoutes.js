5e5c1b24580821a88ac30dab6efa4504a689480d
Avishkar <avishkar1234567890@gmail.com>
2026-03-01 23:27:56 +0530

    feat: connect update, delete and publish routes for posts

--- blog-project/blog-api/src/routes/postRoutes.js
+++ blog-project/blog-api/src/routes/postRoutes.js
@@ -11,4 +11,25 @@ router.get("/", postController.getPublishedPosts);
 router.get("/all", authenticateToken, authorizeAdmin, postController.getAllPosts);
 router.post("/", authenticateToken, authorizeAdmin, postController.createPost);
 
+router.patch(
+    "/:id/publish",
+    authenticateToken,
+    authorizeAdmin,
+    postController.togglePublish
+);
+
+router.put(
+    "/:id",
+    authenticateToken,
+    authorizeAdmin,
+    postController.updatePost
+);
+
+router.delete(
+    ":/id",
+    authenticateToken,
+    authorizeAdmin,
+    postController.deletePost
+)
+
 module.exports = router;
